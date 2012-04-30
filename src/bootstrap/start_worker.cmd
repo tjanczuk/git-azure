42b9f6dda38dda7c41d71b46ffabce62a17a6ca2
Tomasz Janczuk <tomasz@janczuk.org>
2012-04-30 14:36:13 -0700

    fix setup_worker.cmd to provide absolute path to node.exe

--- src/bootstrap/start_worker.cmd
+++ src/bootstrap/start_worker.cmd
@@ -3,5 +3,5 @@
 SET THIS=%~dp0
 
 echo %DATE% %TIME% Starting arr.js runtime
-node.exe %THIS%\repo\.git-azure\src\runtime\arr.js -r %THIS%\repo
+"%programfiles(x86)%\nodejs\"node.exe %THIS%\repo\.git-azure\src\runtime\arr.js -r %THIS%\repo
 echo %DATE% %TIME% Arr.js runtime terminated with code %ERRORLEVEL%
