72a166e4e20a02604997152545f1d590048a3d0f
Tomasz Janczuk <tomasz@janczuk.org>
2012-05-11 15:32:55 -0700

    blob command

---
+++ src/cli/commands/blob.js
@@ -0,0 +1,120 @@
+var fs = require('fs')
+	, path = require('path')
+	, common = require('./common.js')
+	, azure = require('azure');
+
+exports.action = function (cmd) {
+
+	var config;
+
+	function onError(err) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+	}
+
+	function put() {
+		var blob = azure.createBlobService(config.storageAccountName, config.storageAccountKey);
+		if (config.content) {
+			blob.createBlockBlobFromText(config.blobContainerName, config.put, config.content, onError);
+		}
+		else {
+			var fileName = path.resolve(process.cwd(), config.file);
+			blob.createBlockBlobFromFile(config.blobContainerName, config.put, config.file, onError);
+		}
+	}
+
+	function get() {
+		var blob = azure.createBlobService(config.storageAccountName, config.storageAccountKey);
+		if (config.file) {
+			var fileName = path.resolve(process.cwd(), config.file);
+			blob.getBlobToFile(config.blobContainerName, config.get, fileName, onError);
+		}
+		else {
+			blob.getBlobToText(config.blobContainerName, config.get, process.stdout, function (err, text) {
+				onError(err);
+				console.log(text);
+			});
+		}
+	}
+
+	function list() {
+		var blob = azure.createBlobService(config.storageAccountName, config.storageAccountKey);
+		blob.listBlobs(config.blobContainerName, function (err, blobs) {
+			onError(err);
+
+			if (config.verbose) {
+				console.log(blobs);
+			}
+			else {
+				blobs.forEach(function (item) {
+					console.log(item.name);
+				});
+			}
+		});
+	}
+
+	function del() {
+		var blob = azure.createBlobService(config.storageAccountName, config.storageAccountKey);
+		blob.deleteBlob(config.blobContainerName, config.delete, onError);
+	}
+
+	var ops = {
+		put: put,
+		get: get,
+		list: list,
+		'delete': del
+	};
+
+	function checkParametersSpecified() {
+
+		var missing = [];
+
+		['storageAccountName', 'storageAccountKey', 'blobContainerName'].forEach(function (item) {
+			if (!config[item])
+				missing.push('--' + item);
+		});
+
+		var opCount = 0;
+		var op;
+		['get', 'put', 'delete', 'list'].forEach(function (item) {
+			if (config[item]) {
+				opCount++;
+				op = item;
+			}
+		});
+
+		if (opCount === 0) {
+			missing.push('--get, --put, --delete, or --list must be specified');
+		}
+		else if (opCount > 1) {
+			missing.push('--get, --put, --delete, and --list are mutually exclusive');	
+		}
+
+		if (config.put && !config.file && !config.content) {
+			missing.push('--put requires that either --content or --file are specified');
+		}
+
+		if (missing.length > 0) {
+			console.error('The following required parameters must be specified:\n');
+			missing.forEach(console.error);
+			process.exit(1);
+		}
+
+		ops[op]();		
+	}
+
+	common.getAzureConfigFromGit('azure.', ['storageAccountName', 'storageAccountKey', 'blobContainerName'], function (err, gitConfig) {
+		if (err) {
+			console.error(err.toString());
+			process.exit(1);
+		}
+
+		config = gitConfig;
+		common.merge(cmd, config, ['put', 'get', 'delete', 'list', 'file', 'content',
+			'storageAccountName', 'storageAccountKey', 'blobContainerName', 'verbose']);
+
+		checkParametersSpecified();
+	});
+}
--- src/cli/git-azure.js
+++ src/cli/git-azure.js
@@ -6,14 +6,14 @@ var program = require('commander')
     , util = require('util')
     , colors = require('colors');
 
-var oldError = console.error
+var oldError = console.error;
 console.error = function (thing) {
     if (typeof thing === 'string')
-        thing = thing.red
-    return oldError.call(this, thing)
+        thing = thing.red;
+    return oldError.call(this, thing);
 }
 
-program.version(require(path.resolve(__dirname, '../../package.json')).version)
+program.version(require(path.resolve(__dirname, '../../package.json')).version);
 
 program.command('init')
     .description('One-time initialization of a Windows Azure Service associated with this Git repo.'.cyan)
@@ -29,8 +29,9 @@ program.command('init')
     .option('-b, --blobContainerName <name>', '[optional] name of the Windows Azure Blob Storage contaniner to create or use')
     .option('-r, --remote <name>', '[optional] remote name to push git-azure runtime scaffolding to (defaults to origin)')
     .option('-t, --branch <name>', '[optional] branch name to push git-azure runtime scaffolding to (defaults to master)')
-    .option('-t, --postReceive <url_path>', '[optional] obfuscated URL path for the post receive hook endpoint')
-    .action(require('./commands/init.js').action)
+    .option('-c, --postReceive <url_path>', '[optional] obfuscated URL path for the post receive hook endpoint')
+    .option('-h, --noCache', '[optional] do not cache settings in Git config after successful completion')
+    .action(require('./commands/init.js').action);
 
 program.command('app <name>')
     .description('Manage node.js applications associated with this Git repo.'.cyan)
@@ -41,21 +42,21 @@ program.command('app <name>')
     .option('-c, --cert <blob_name>', 'the Windows Azure Blob name with PKCS#7 encoded (PEM) X.509 certificate for SSL')
     .option('-k, --key <blob_name>', 'the Windows Azure Blob name with PKCS#7 encoded (PEM) private key for SSL')
     .option('-d, --delete', 'if --host specified, delete the host entry only; otherwise delete the entire application')
-    .action(require('./commands/app.js').action)
+    .action(require('./commands/app.js').action);
 
 program.command('blob')
     .description('Manipulate data in Azure Blob Storage.'.cyan)
-    .option('-a, --add <name> [data]', 'add data to blob storage')
+    .option('-p, --put <name> [data]', 'add or override data in blob storage')
     .option('-g, --get <name>', 'get data from blob storage')
     .option('-d, --delete <name>', 'delete data from blob storage')
     .option('-l, --list', 'list blobs in blob storage')
-    .option('-f, --file', 'optionally use with -g or -a options to indicate file to save or read data from')
-    .option('-n, --storageAccountName <name>', 'name of the Windows Azure Blob Storage account to use')
-    .option('-k, --storageAccountKey <key>', 'access key for the specified Windows Azure Blob Storage account')
-    .option('-b, --blobContainerName <name>', 'name of the Windows Azure Blob Storage contaniner to create or use')
-    .action(function (cmd) {
-        console.log('blob: ', cmd);
-    })
+    .option('-f, --file <name>', 'optionally use with --get or --put options to indicate file to save to or read from')
+    .option('-c, --content <text>', 'specifies content of the blob for --put; --content takes precedence over --file')
+    .option('-v, --verbose, ', 'more verbose output')
+    .option('-a, --storageAccountName <name>', '[required] name of the Windows Azure Blob Storage account to use')
+    .option('-k, --storageAccountKey <key>', '[required] access key for the specified Windows Azure Blob Storage account')
+    .option('-b, --blobContainerName <name>', '[required] name of the Windows Azure Blob Storage contaniner to create or use')
+    .action(require('./commands/blob.js').action);
 
 program.command('restart')
     .description('Restart the Windows Azure service associated with this Git repo.'.cyan)
@@ -66,7 +67,7 @@ program.command('restart')
     .option('-q, --quiet', 'succeed or fail without showing prompts')
     .action(function (cmd) {
         console.log('restart: ', cmd);
-    })
+    });
 
 program.command('destroy')
     .description('Destroy the Windows Azure service associated with this Git repo.'.cyan)
@@ -79,9 +80,9 @@ program.command('destroy')
     .option('-q, --quiet', 'succeed or fail without showing prompts')
     .action(function (cmd) {
         console.log('destroying: ', cmd);
-    })
+    });
 
 if (process.argv.length == 2)
-    program.parse(['', '', '-h'])
+    program.parse(['', '', '-h']);
 else
-    program.parse(process.argv)
+    program.parse(process.argv);
