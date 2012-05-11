b2bc31e769f8156ce5d62531d92b63f40e3cb3cb
Tomasz Janczuk <tomasz@janczuk.org>
2012-05-10 17:32:46 -0700

    fix #15: reduce ads

--- src/cli/commands/common.js
+++ src/cli/commands/common.js
@@ -23,24 +23,27 @@ exports.startWait = function () {
 	return setInterval(showWait, 1000);
 };
 
+var ads = [
+	'Did you know you can easily access Windows Azure services from node.js?\nCheck out https://github.com/WindowsAzure/azure-sdk-for-node for details.',
+	'Did you know you can store data in Windows Azure using blob storage, table storage, and MS SQL?\nVisit https://www.windowsazure.com to learn more.',
+	'Did you know you can host node.js applications in IIS along with other types of content?\nVisit https://github.com/tjanczuk/iisnode to get started.',
+	'Did you know you can develop node.js applications in WebMatrix on Windows?\nDownload it for free at http://www.microsoft.com/web/webmatrix/',
+	'Did you know Windows Azure SDK for node.js has everything you need to develop node.js apps on Windows?\nCheck out https://www.windowsazure.com/en-us/develop/nodejs/ for more.',
+	'Did you know that node.js developers live on average 2.7 years longer than PHP devs? [Quotation needed]',
+	'[Place for your ad] Send money and the text of the ad to @tjanczuk. Seriously.'
+];
+
 exports.startAds = function () {
 
-	var ads = [
-		'Did you know you can easily access Windows Azure services from node.js?\nCheck out https://github.com/WindowsAzure/azure-sdk-for-node for details.',
-		'Did you know Windows Azure offers three storage services for your data: blob storage, table storage, and MS SQL?\nVisit https://www.windowsazure.com to learn more.',
-		'Did you know Windows Azure has a ' + '90-day free trial'.green + '?\nCheck out https://www.windowsazure.com for details.',
-		'Did you know you can host node.js applications in IIS along with other types of content?\nVisit https://github.com/tjanczuk/iisnode to get started.',
-		'Did you know you can develop node.js applications in WebMatrix on Windows?\nDownload it for free at http://www.microsoft.com/web/webmatrix/',
-		'Did you know Windows Azure SDK for node.js has everything you need to develop node.js apps on Windows?\nCheck out https://www.windowsazure.com/en-us/develop/nodejs/ for more.',
-		'Did you know that node.js developers live on average 2.7 years longer than PHP devs? [Quotation needed]',
-		'[Place for your ad] Send money and the text of the ad to @tjanczuk. Seriously.'
-	];
-
 	var showAd = function () {
-		console.log(ads[Math.floor(Math.random() * ads.length)]);
+		if (ads.length > 0) {
+			var index = Math.floor(Math.random() * ads.length);
+			console.log(ads[index]);
+			ads.splice(index, 1);
+		}
 	}
 
-	return setInterval(showAd, 16000);
+	return setInterval(showAd, 60000);
 };
 
 exports.httpsRequest = 	function (subscription, cert, host, url, method, body, headers, isLongLasting, callback) {
